4ed0e7f01c53b3722f2ac75b58d8b7502558f347
Tim Sneath <tim@sneath.org>
2017-05-04 10:05:39 -0700

    Update for map API changes
    ...also subset the permitting data so that it renders better

--- seattle_building_permits.R
+++ seattle_building_permits.R
@@ -1,44 +1,41 @@
-# seattle_building_permits.R | last updated: 5/19/15 | Tim Sneath
+# seattle_building_permits.R | last updated: 5/4/17 | Tim Sneath
 
 ## Seattle building permits data
 file.url <- "https://data.seattle.gov/api/views/mags-97de/rows.csv?accessType=DOWNLOAD"
-download.file(file.url, destfile="permits.csv")
+download.file(file.url, destfile = "permits.csv")
 
 ## Read data in and massage formats a little
-permits <- read.csv("permits.csv", stringsAsFactors=FALSE)
+permits <- read.csv("permits.csv", stringsAsFactors = FALSE)
 permits$Category <- factor(permits$Category)
-permits$Value <- as.numeric(substr(permits$Value, 2, nchar(permits$Value)-1))
-permits$Application.Date <- as.Date(permits$Application.Date, format="%m/%d/%Y")
+permits$Value <- as.numeric(substr(permits$Value, 2, nchar(permits$Value) - 1))
+permits$Application.Date <- as.Date(permits$Application.Date, format = "%m/%d/%Y")
+
+## Subset to just the last three months and ignore uncategorized permits
+recentPermits <- subset(permits, Application.Date > (Sys.Date() - 90) &
+                                 Category != "")
 
 library(ggmap)
 library(mapproj)
 
 ## Establish a bounding box for the permits data
-bbox <- c(min(permits$Longitude, na.rm=TRUE), 
-          min(permits$Latitude, na.rm=TRUE), 
-          max(permits$Longitude, na.rm=TRUE), 
-          max(permits$Latitude, na.rm=TRUE))
-
-## Another alternative - zooming a little closer
-## bbox <- c(quantile(permits$Longitude, probs=0.1, na.rm=TRUE), 
-##           quantile(permits$Latitude, probs=0.25, na.rm=TRUE), 
-##           quantile(permits$Longitude, probs=0.9, na.rm=TRUE), 
-##           quantile(permits$Latitude, probs=0.75, na.rm=TRUE))
+bbox <- c(min(recentPermits$Longitude, na.rm = TRUE),
+          min(recentPermits$Latitude, na.rm = TRUE),
+          max(recentPermits$Longitude, na.rm = TRUE),
+          max(recentPermits$Latitude, na.rm = TRUE))
 
 names(bbox) <- NULL
 
-## Grab map from OpenStreetMaps.
-map <- get_map(location=bbox,
-               zoom=12, scale=OSM_scale_lookup(zoom=12),
-               maptype="roadmap", 
-               source="osm")
-
+## Grab map from Google Maps
+map <- get_map(location = bbox,
+               scale = 2,
+               maptype = "roadmap",
+               source = "google")
 
 ## ggplot2-like syntax - create a map and then plot points appropriately
-ggmap(map) + geom_point(aes(x=permits$Longitude, 
-                            y=permits$Latitude, 
-                            colour=factor(permits$Category),
-                            size=sqrt(permits$Value)),
-                        data=permits, 
-                        # alpha=.5, 
-                        na.rm=TRUE)
+ggmap(map) + geom_point(aes(x = recentPermits$Longitude,
+                            y = recentPermits$Latitude,
+                            colour = factor(recentPermits$Category),
+                            size = sqrt(recentPermits$Value)),
+                        data  = recentPermits,
+                        alpha = .5, 
+                        na.rm = TRUE)
